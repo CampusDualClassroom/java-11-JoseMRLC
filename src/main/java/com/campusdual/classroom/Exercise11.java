105702438d379dad4fd95e42d444d4116b2cf3cb
JoseMRLC <josemriveiroticbgmail.com>
2024-09-25 12:35:50 +0200

    Ejercicio Resuelto

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,31 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
-    }
 
+        redRemote.turnOn();
+
+        System.out.println("Subiendo canal:");
+        redRemote.channelUp();
+        System.out.println("Canal actual: " + redRemote.channel);
+        System.out.println("Subiendo volumen:");
+        redRemote.volumeUp();
+        System.out.println("Volumen actual: " + redRemote.volume);
+
+        System.out.println("Bajando volumen:");
+        for (int i = 0; i < 21; i++) {
+            redRemote.volumeDown();
+            System.out.println("Volumen actual: " + redRemote.volume);
+        }
+
+        System.out.println("Estableciendo canal en 0:");
+        redRemote.channelDown();
+        System.out.println("Canal actual: " + redRemote.channel);
+
+        System.out.println("Bajando canal:");
+        redRemote.channelDown();
+        System.out.println("Canal actual: " + redRemote.channel);
+
+        redRemote.turnOff();
+        System.out.println("La TV está apagada.");
+    }
 }
